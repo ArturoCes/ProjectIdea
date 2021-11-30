b043c8aee939ea530f00897fe8b4710ad8d8c131
Arturo Céspedes Pedrazas <Arturo200212@gmail.com>
2021-11-30 09:57:04 +0100

    Second Commit

---
+++ PrimerProyectoIdea/src/com/salesianostriana/dam/Alumno.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+
+
+    public Alumno(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
