eedbca6a2a0438bc2dffbd8c2fd2eab0778f4a5a
Arturo Céspedes Pedrazas <Arturo200212@gmail.com>
2021-11-30 09:54:15 +0100

    First Commit rama2

---
+++ PrimerProyectoIdea/src/com/salesianostriana/dam/Main.java
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hola mundo");
+        System.out.println("Como estan?");
+        System.out.println("PODER");
+    }
+}
